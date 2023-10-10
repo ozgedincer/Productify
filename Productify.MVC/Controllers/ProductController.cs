08ee767990de9fddecc4cbab132b624b17f56a2c
Özge Dinçer <dncerozge@gmail.com>
2023-10-10 09:00:11 +0300

    Proje dosyası ekle.

---
+++ Productify.MVC/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Productify.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        List<Product> _products = new()
+        {
+            new Product("Laptop Computer"),
+            new Product("Smartphone"),
+            new Product("Wireless Headphones"),
+            new Product("Digital Camera"),
+            new Product("Tablet")
+        };
+        public IActionResult GetAll()
+        {
+            return View(_products);
+        }
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+    }
+}
